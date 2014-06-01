146e0ac51a3129ee9d580c54034b36187ce9bbe8
comodojo <devel@comodojo.org>
2014-06-02 00:28:15 +0200

    working on 3.0 (1)

--- lib/comodojo/trace.php
+++ lib/comodojo/trace.php
@@ -1,39 +1,92 @@
 <?php
 
-	/**
-	 * Trace request/response in the specified log file
-	 */
-	private function trace() {
+/**
+ * Produce request trace on file.
+ * 
+ * @package		Comodojo Spare Parts
+ * @author		comodojo.org
+ * @copyright	__COPYRIGHT__ comodojo.org (info@comodojo.org)
+ * @version		__CURRENT_VERSION__
+ * @license		GPL Version 3
+ */
 
-		if ($this->isTrace || GLOBAL_TRANSACTION_TRACING_ENABLED) {
+class trace {
 
-			$myMessage = "****** REQUEST FROM " . $_SERVER["REMOTE_ADDR"] . " AT " . date("d-m-Y (D) H:i:s",time()) . " ******\n";
-			$myMessage .= "- Client request's method: ".$_SERVER['REQUEST_METHOD']."\n";
-			$myMessage .= "- Client sent: \n";
-			foreach ($_GET as $parameter=>$value) {
-				if (in_array($parameter,$this->requiredParameters)) $myMessage .= "[".$parameter."]* => ".$value."\n"; 
-				else $myMessage .= "[".$parameter."] => ".$value."\n";
+	private $trace_path = getcwd()."/../".TRANSACTION_TRACES_PATH;
+
+	private $service = false;
+
+	private $trace_content = '';
+
+	public function __construct($service) {
+
+		if ($this->trace_path[strlen($this->trace_path)-1] != "/") $this->trace_path = $this->trace_path . "/";
+
+		$current_time = date("d-m-Y (D) H:i:s",time());
+
+		$this->service = empty($service) ? "unknown" : $service;
+
+		$this->trace_content .= "****** TRACE START ******\n";
+
+		$this->trace_content .= " >> REQUEST FROM " . $_SERVER["REMOTE_ADDR"] . " AT " . $current_time . " <<\n";
+
+	}
+
+	public function __destruct() {
+
+		$this->trace_content .= "****** TRACE END ******\n";
+
+		try {
+			$this->write_content();	
+		} catch (Exception $e) {
+			//debug something here!
+		}
+
+	}
+
+	public function client($method, $parameters, $required) {
+
+		$this->trace_content .= "- Client request's method: " . $method . "\n";
+
+		$this->trace_content .= "- Client sent: \n";
+			
+		foreach ($parameters as $parameter=>$value) {
+			
+			if (in_array($parameter,$required)) {
+				$this->trace_content .= "[".$parameter."]* => ".$value."\n"; 
 			}
-			$myMessage .= "- Server reply with status code: ".$this->statusCode."\n";
-			$myMessage .= "- Server returns (".$this->transport."): \n";
-			$myMessage .= $this->toReturn;
-			$myMessage .= "\n****** REQUEST END ******\n";
-
-			try {
-				if (!$fh = fopen(getcwd()."/../".TRANSACTION_TRACES_PATH.$this->logFile, 'a')) {
-					throw new Exception('Could not open log file!');
-				}
-				if (!$fw = fwrite($fh, $myMessage)) {
-					throw new Exception('Could not write log file!');
-				}
-				fclose($fh);
-			}
-			catch (Exception $e) {
-				$this->debug($e);
+			else {
+				$this->trace_content .= "[".$parameter."] => ".$value."\n";
 			}
 
 		}
 
 	}
+
+	public function server($status, $transport, $data) {
+
+		$this->trace_content .= "+ Server reply with status code: ".$status."\n";
+		$this->trace_content .= "+ Server returns (".$transport."): \n";
+		$this->trace_content .= var_export($data, true) . "\n";
+
+	}
+
+	private function write_content() {
+
+		try {
+			if (!$fh = fopen($this->trace_path.$this->service.".trace", 'a')) {
+				throw new comodojoException('Could not open log file!');
+			}
+			if (!$fw = fwrite($fh, $this->trace_content)) {
+				throw new comodojoException('Could not write log file!');
+			}
+			fclose($fh);
+		}
+		catch (comodojoException $e) {
+			throw $e;
+		}
+	}
+
+}
 
 ?>
