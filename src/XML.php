29a350e73148ec3a61a9cd4b8a78f95c080e2b23
comodojo <info@comodojo.org>
2014-07-24 22:29:20 +0200

    XML transformation class recoded

--- src/XML.php
+++ src/XML.php
@@ -1,13 +1,14 @@
-<?php namespace comodojo\Dispatcher;
+<?php namespace Comodojo\Dispatcher;
+
+use \SimpleXMLElement;
+use \Comodojo\Exception\XMLException;
 
 /** 
- * XML.php
- * 
- * XML data transformation class;
+ * XML data transformation class
  *
- * @package		Comodojo dispatcher (Spare Parts)
- * @author		comodojo <info@comodojo.org>
- * @license		GPL-3.0+
+ * @package     Comodojo dispatcher (Spare Parts)
+ * @author      comodojo <info@comodojo.org>
+ * @license     GPL-3.0+
  *
  * LICENSE:
  * 
@@ -24,253 +25,380 @@
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
-
 class XML {
-	
-	/**
-	 * Source array to transform
-	 * 
-	 * @var		string
-	 * @default	false	(will return error)
-	 */
-	public $sourceArray = false;
-	
-	/**
-	 * XML version (header)
-	 * 
-	 * @var 	string
-	 * @default	1.0	
-	 */
-	public $xmlVersion = "1.0";
-	
-	/**
-	 * XML encoding (header)
-	 * 
-	 * @var 	string
-	 * @default	UTF-8	
-	 */
-	public $xmlEncoding = DISPATCHER_DEFAULT_ENCODING;
-	
-	/**
-	 * XML parent element (XML body)
-	 * 
-	 * @var 	string
-	 * @default	content	
-	 */
-	public $parentElement = "content";
-	
-	/**
-	 * XML output string should include header?
-	 * 
-	 * @var 	bool
-	 * @default	true	
-	 */
-	public $includeHeader = true;
-	
-	/**
-	 * XML source string to transform (in array)
-	 * 
-	 * @var 	string
-	 * @default	false	(will return error)
-	 */
-	public $sourceString = false;
-	
-	/**
-	 * XML source encoding 
-	 * 
-	 * @var 	string
-	 * @default	UTF-8	
-	 */
-	public $sourceEncoding = DISPATCHER_DEFAULT_ENCODING;
-	
-	/**
-	 * XML destination encoding 
-	 * 
-	 * @var 	string
-	 * @default	UTF-8	
-	 */
-	public $destinationEncoding = DISPATCHER_DEFAULT_ENCODING;
-	
-	public $caseFolding = 0;
-	public $skipWhite = 1;
-	
-	private $_xmlobj;
-	private $_parser;
-	private $_counter;
-	private $_struct = array();
-	private $_unique = array();
-	private $result = null;
-	
-	private function throwSuccess() {
-		return $this->result;
-	}
-	
-	private function throwFailure($reason) {
-		return $reason;
-	}
-	
-	/**
-	 * encode array in XML string 
-	 * 
-	 * @return string	
-	 */
-	public function encode() {
 
-		if (!$this->sourceArray OR !is_array($this->sourceArray)) return $this->throwFailure('No source array!');
-		
-		$xmltext = ($this->includeHeader ? $this->_getHeader() : "") . /*($this->startFromParent ?*/ $this->_getParent() /*: "")*/; 
-		
-		$this->_xmlobj = new \SimpleXMLElement($xmltext);
-	
-		$this->_encode($this->_xmlobj, $this->sourceArray);
-		
-		return $this->_xmlobj->asXML();
-		
-	}
+    /**
+     * XML version
+     * 
+     * @var string
+     */
+    private $version = "1.0";
+    
+    /**
+     * XML encoding (header)
+     * 
+     * @var string
+     */
+    private $encoding = null;
+    
+    /**
+     * XML parent element (XML body)
+     * 
+     * @var string  
+     */
+    private $parent = "content";
+    
+    /**
+     * Include/exclude header in XML construction
+     * 
+     * @var bool
+     */
+    private $includeHeader = true;
+    
+    /**
+     * Controls whether case-folding is enabled for XML parser
+     * 
+     * @var integer
+     */
+    private $caseFolding = 0;
+    
+    /**
+     * Whether XML parser should skip values consisting of whitespace characters. 
+     * 
+     * @var integer
+     */
+    private $skipWhite = 1;
+    
+    /**
+     * Header string to prepend to xml data
+     * 
+     * @var string
+     */
+    private $header_string = '<?xml version="__VERSION__" encoding="__ENCODING__"?>';
 
-	private function _getHeader() {
-		return "<?xml version=\"$this->xmlVersion\" encoding=\"$this->xmlEncoding\"?>";
-	}
-	
-	private function _getParent() {
-		return "<".$this->parentElement."></".$this->parentElement.">";
-	}
-	
-	private function _encode($xmlObj, $sourceArray) {
-		foreach ($sourceArray as $k => $v) {
-			if (is_array($v)) {
-				$this->_encode($xmlObj->addChild(is_numeric($k) ? 'KEY_'.$k : $k), $v);
-			}
-			else {
-				$xmlObj->addChild(is_numeric($k) ? 'KEY_'.$k : $k, $v);
-			}
-		}
-		return $xmlObj;
-	}
+    /**
+     * Internal pointer to SimpleXMLElement
+     * 
+     */
+    private $ObjectXML;
 
-	/**
-	 * encode XML string in array 
-	 * 
-	 * @return array	
-	 */
-	public function decode() {
-		if ($this->sourceEncoding) {
-			$this->_parser = xml_parser_create('');
-		}
-		else {
-			$this->_parser = xml_parser_create($this->sourceEncoding);
-		}
-		if ($this->destinationEncoding !== false) xml_parser_set_option($this->_parser,XML_OPTION_TARGET_ENCODING,$this->destinationEncoding);
-		
-		xml_parser_set_option($this->_parser,XML_OPTION_CASE_FOLDING,$this->caseFolding);
-		xml_parser_set_option($this->_parser,XML_OPTION_SKIP_WHITE,$this->skipWhite);
-		
-		//$pointer = &$this->_struct;
-		
-		xml_parse_into_struct($this->_parser,$this->sourceString,$this->_struct, $index);
-		
-		$this->_counter = count($this->_struct);
-		
-		array_walk($this->_struct, array ($this, '_sanitizeKeys'));
-		
-		//print_r($this->_struct);
-		
-		$this->_free();
-		
-		$t = $this->_getTree();
-		
-		return $t; 
-		
-	}
-	
-	private function _sanitizeKeys($input, $key) {
-		$kVal = explode("_",$this->_struct[$key]["tag"]);
-		if ($kVal[0] == "KEY") {
-			$this->_struct[$key]["tag"] = intval($kVal[1]);
-		}
-	}
-	
-	private function _free() {
-		if( isset($this->_parser) AND is_resource($this->_parser)) {
-			xml_parser_free($this->_parser);
-			unset($this->parser);
-		}
-	}
-	
-	private function _getTree() {
-		$i = 0;
-		$tree = array();
-		//comodojo_debug($this->_struct);
-		$_key = $this->_struct[$i]["tag"];
-		$_attributes = isset($this->_struct[$i]["attributes"]) ? $this->_struct[$i]["attributes"] : "";
-		$_value = isset($this->_struct[$i]["value"]) ? $this->_struct[$i]["value"] : "";
-		$_child = $this->_getChild($i);
-		$tree = $this->_addNode($tree, $_key, $_attributes, $_child, $_value);
-		unset($this->struct);
-		return $tree;
-	}
+    /**
+     * Internal pointer to xml_parser
+     * 
+     */
+    private $parser;
 
-	private function _getChild(&$i) {
-		$_children = array();
-		while (++$i < $this->_counter) {
-			$_tagname = $this->_struct[$i]["tag"];
-			$_attributes = isset($this->_struct[$i]["attributes"]) ? $this->_struct[$i]["attributes"] : "";
-			$_value = isset($this->_struct[$i]["value"]) ? $this->_struct[$i]["value"] : "";
-			switch($this->_struct[$i]["type"]) {
-				case "open":
-					$_child = $this->_getChild($i);
-					$_children = $this->_addNode($_children,$_tagname,$_attributes,$_child,$_value);
-					break;
-				case "complete":
-					$_child = "";
-					$_children = $this->_addNode($_children,$_tagname,$_attributes,$_child,$_value);
-					break;
-				case "close":
-					return $_children;
-					break;
-			}
-		}
-	}
+    /**
+     * Internal pointer to parsed xml structs
+     * 
+     * @var array
+     */
+    private $struct = array();
 
-	private function _addNode($target, $key, $attributes, $child, $value) {
-		if (!isset($target[$key])) {
-			/*if (in_array($key,$this->_unique)) {
-				if ($child != "") {
-					 $target[$key][0] = $child;
-				}
-				if ($attributes != "") {
-					foreach($attributes as $k => $v) $target[$key][0][$k] = $v;
-				}
-				elseif ($value != "") $target[$key][0] = $value;
-				$index=1;
-			}
-			else {*/
-				if ($child != "") {
-					$target[$key] = $child;
-				}
-				if ($attributes != ""){
-					foreach($attributes as $k => $v) $target[$key][$k] = $v;
-				}
-				elseif ($value != "") $target[$key] = $value;
-			//}
-		}
-		else {
-			if (!isset($target[$key][0])) {
-				$oldval = $target[$key];
-				$target[$key] = array();
-				$target[$key][0] = $oldval;
-				$index=1;
-			}
-			else {
-				$index = count($target[$key]);
-			}
-			if ($child != "") $target[$key][$index] = $child;
-			if ($attributes != "") foreach($attributes as $k => $v) $target[$key][$index][$k] = $v;
-			elseif ($value != "") $target[$key][$index] = $value;
-		}
-		return $target;
-	}
-	
+    /**
+     * Number of elements retrieved from parser
+     * 
+     * @var integer
+     */
+    private $counter;
+    
+    /**
+     * XML class constructor
+     */
+    final public function __construct() {
+
+        $this->encoding = defined('DISPATCHER_DEFAULT_ENCODING') ? DISPATCHER_DEFAULT_ENCODING : "UTF-8";
+
+    }
+
+    /**
+     * Set if header should be included in xml encoding 
+     * 
+     * @param bool $bool
+     *
+     * @return Object $this
+     */
+    final public function setIncludeHeader($bool) {
+
+        $this->includeHeader = filter_var($bool, FILTER_VALIDATE_BOOLEAN);
+
+        return $this;
+
+    }
+
+    /**
+     * Enable case-folding for XML parser
+     * 
+     * @param int $int
+     *
+     * @return Object $this
+     */
+    final public function setCaseFolding($int) {
+
+        $this->caseFolding = filter_var($int, FILTER_VALIDATE_INT);
+
+        return $this;
+
+    }
+
+    /**
+     * Enable whitespace characters skip for XML parser
+     * 
+     * @param int $int
+     *
+     * @return Object $this
+     */
+    final public function setSkipWhite($int) {
+
+        $this->skipWhite = filter_var($int, FILTER_VALIDATE_INT);
+
+        return $this;
+
+    }
+
+    /**
+     * Set xml parser and encoder encoding
+     * 
+     * @param string $encoding
+     *
+     * @return Object $this
+     */
+    final public function setEncoding($encoding) {
+
+        $encoding = strtoupper($encoding);
+
+        $this->encoding = in_array($encoding, mb_list_encodings()) ? $encoding : $this->encoding;
+
+        return $this;
+
+    }
+
+    /**
+     * Get XML header
+     *
+     * @return  string
+     */
+    final public function getHeader() {
+
+        return str_replace(array("__VERSION__", "__ENCODING__"), array($this->version, $this->encoding), $this->header_string);
+
+    }
+    
+    /**
+     * Get XML header
+     *
+     * @return  string
+     */
+    final public function getParent() {
+
+        return "<".$this->parent."></".$this->parent.">";
+
+    }
+
+    /**
+     * Encode array to XML string 
+     * 
+     * @return string   
+     */
+    public function encode(array $data) {
+
+        $structure = ($this->includeHeader ? $this->getHeader() : "") . $this->getParent();
+        
+        $this->ObjectXML = new SimpleXMLElement($structure);
+        
+        $this->pushElement($this->ObjectXML, $data);
+        
+        $xml = $this->ObjectXML->asXML();
+
+        if ( $xml === false ) throw new XMLException("Error formatting object");
+        
+        return $xml;
+        
+    }
+
+    /**
+     * encode XML string into array 
+     * 
+     * @return array    
+     */
+    public function decode(string $xml, $encoding=null) {
+
+        $this->parser = is_null($encoding) ? xml_parser_create() : xml_parser_create($encoding);
+
+        xml_parser_set_option($this->parser,XML_OPTION_TARGET_ENCODING,$this->encoding);
+        xml_parser_set_option($this->parser,XML_OPTION_CASE_FOLDING,$this->caseFolding);
+        xml_parser_set_option($this->parser,XML_OPTION_SKIP_WHITE,$this->skipWhite);
+        
+        $parser_structs = xml_parse_into_struct($this->parser, $xml, $this->struct, $index);
+        
+        if ( $parser_structs === 0 ) throw new XMLException("Failed to parse xml data into structs");
+        
+        $this->counter = count($this->struct);
+
+        array_walk($this->struct, array($this, 'sanitizeKeys'));
+        
+        $this->free();
+        
+        $t = $this->getTree();
+        
+        return $t; 
+        
+    }
+    
+    /**
+     * convert keys created from XML::encode() into array numeric keys
+     * 
+     */
+    private function sanitizeKeys($input, $key) {
+
+        $key_value = explode("_",$this->struct[$key]["tag"]);
+
+        if ( sizeof($key_value) == 2 AND $key_value[0] == "KEY" AND is_numeric($key_value[1]) ) {
+
+            $this->struct[$key]["tag"] = intval($key_value[1]);
+
+        }
+
+    }
+    
+    /**
+     * Free xml parser and unset resource
+     */
+    private function free() {
+
+        if( isset($this->_parser) AND @is_resource($this->_parser) ) {
+
+            xml_parser_free($this->parser);
+
+            unset($this->parser);
+
+        }
+        
+    }
+    
+    /**
+     * Get composed tree into array and unset resource
+     *
+     * @return array
+     */
+    private function getTree() {
+
+        $i = 0;
+
+        $tree = array();
+
+        $key = $this->struct[$i]["tag"];
+
+        $attributes = isset($this->struct[$i]["attributes"]) ? $this->struct[$i]["attributes"] : "";
+
+        $value = isset($this->struct[$i]["value"]) ? $this->struct[$i]["value"] : "";
+
+        $child = $this->getChilds($i);
+
+        $tree = $this->addNode($tree, $key, $attributes, $child, $value);
+
+        unset($this->struct);
+
+        return $tree;
+
+    }
+
+    /**
+     * Get element childs (if any)
+     *
+     * @return array
+     */
+    private function getChilds(&$i) {
+
+        $children = array();
+
+        while (++$i < $this->counter) {
+
+            $tagname = $this->struct[$i]["tag"];
+
+            $attributes = isset($this->struct[$i]["attributes"]) ? $this->struct[$i]["attributes"] : "";
+
+            $value = isset($this->struct[$i]["value"]) ? $this->struct[$i]["value"] : "";
+
+            switch($this->struct[$i]["type"]) {
+
+                case "open":
+                $child = $this->getChilds($i);
+                $children = $this->addNode($children, $tagname, $attributes, $child, $value);
+                break;
+                
+                case "complete":
+                $child = "";
+                $children = $this->addNode($children, $tagname, $attributes, $child, $value);
+                break;
+                
+                case "close":
+                return $children;
+                break;
+
+            }
+
+        }
+
+    }
+
+    /**
+     * Add node to array
+     *
+     */
+    private function addNode($target, $key, $attributes, $child, $value) {
+        
+        if ( !isset($target[$key]) ) {
+            
+            if ($child != "") $target[$key] = $child;
+
+            if ($attributes != "") foreach($attributes as $akey => $avalue) $target[$key][$akey] = $avalue;
+
+            else if ( $value != "" ) $target[$key] = $value;
+            
+        } else {
+
+            if (!isset($target[$key][0])) {
+                
+                $oldval = $target[$key];
+                $target[$key] = array();
+                $target[$key][0] = $oldval;
+                $index=1;
+
+            }
+            else {
+
+                $index = count($target[$key]);
+
+            }
+
+            if ( $child != "" ) $target[$key][$index] = $child;
+
+            if ( $attributes != "" ) foreach($attributes as $akey => $avalue) $target[$key][$index][$akey] = $avalue;
+
+            elseif ( $value != "" ) $target[$key][$index] = $value;
+        }
+
+        return $target;
+
+    }
+
+    /**
+     * Push key/value element into SimpleXMLElement
+     *
+     * @param SimpleXMLElement $ObjectXML
+     * @param array            $data
+     */
+    private function pushElement($ObjectXML, $data) {
+
+        foreach ($data as $key => $value) {
+
+            if ( is_array($value) ) $this->pushElement($ObjectXML->addChild(is_numeric($key) ? 'KEY_'.$key : $key), $value);
+
+            else $ObjectXML->addChild(is_numeric($key) ? 'KEY_'.$key : $key, mb_convert_encoding($value, $this->encoding));
+
+        }
+
+    }
+    
 }
-
-?>
