866ac09243422205d815bdcb71d0cacfc39afd47
marcogiovinazzi <marco.giovinazzi@comodojo.org>
2016-02-06 15:39:42 +0100

    get whole config with no property

--- src/Components/Configuration.php
+++ src/Components/Configuration.php
@@ -54,9 +54,13 @@ class Configuration {
 
     }
 
-    final public function get($property) {
+    final public function get($property = null) {
 
-        if (array_key_exists($property, $this->attributes)) {
+        if ( is_null($property) ) {
+            
+            return $this->attributes;
+            
+        } else if (array_key_exists($property, $this->attributes)) {
 
             $value = $this->attributes[$property];
 
@@ -82,9 +86,11 @@ class Configuration {
 
             return $value;
 
+        } else {
+            
+            return null;
+            
         }
-
-        return null;
 
     }
 
